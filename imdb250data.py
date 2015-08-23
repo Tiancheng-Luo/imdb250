4217a56e1aa3165ac1701a02095f7c3d30e5bca8
Ying R Zhao <jingle3276@hotmail.com>
2015-08-23 17:14:12 -0400

    index rated

--- imdb250data.py
+++ imdb250data.py
@@ -5,6 +5,7 @@ from contextlib import closing
 
 
 def request_data(imdb_id):
+    # make request to imdb api
     url = 'http://www.omdbapi.com/?i={}&plot=short&r=json'.format(imdb_id)
     r = requests.get(url)
     return r.json()
@@ -12,10 +13,10 @@ def request_data(imdb_id):
 
 def top_250_ids():
     with closing(open('top250.txt')) as f:
-        return [line.rstrip('\r\n') for line in f]
+        return [(i+1, line.rstrip('\r\n')) for i, line in enumerate(f)]
 
 
 def fetch_movies():
-    ids = top_250_ids()[:10]
-    movies = [request_data(id) for id in ids]    
+    ids = top_250_ids()
+    movies = [(i, request_data(id)) for i, id in ids]
     return movies
--- index.py
+++ index.py
@@ -8,11 +8,13 @@ import xapian as _x
 from imdb250data import fetch_movies
 
 
+FIELDS = ['Title', 'Plot', "Actors", "Director", "Year", "Rated"]
+RATED = ["G", "PG", "PG-13", "R"]
 
-
-def _index_id(imdb_id):
-    id_str = re.match('.*?([0-9]+)$', imdb_id).group(1)
-    return int(id_str)
+def _format_rated(rated):
+    if rated not in RATED:
+        return "N/A"
+    return rated
 
 def main():
     movies = fetch_movies()
@@ -26,13 +28,13 @@ def main():
 
     with closing(_x.WritableDatabase('./xdb/movies.db',
                                      _x.DB_CREATE_OR_OPEN)) as x_db:
-        for mov in movies:
+        for rank, mov in movies:
             # make a new document
             x_doc = _x.Document()
             imdb_id = mov.get(u'imdbID')
 
             # Store the selected fields for display purposes.
-            data = {sel: mov[sel] for sel in ['Title', 'Plot', "Actors"]}
+            data = {sel: mov[sel] for sel in FIELDS}
             x_doc.set_data(json.dumps(data, encoding='utf8'))
 
             # setup indexer
@@ -43,18 +45,26 @@ def main():
             title = mov.get(u"Title")
             plot = mov.get(u"Plot")
             actors = mov.get(u'Actors')
+            directors = mov.get(u"Director")
+            year = mov.get(u'Year')
+            rated = mov.get(u'Rated')
             
             x_doc.add_term(imdb_id)
             indexer.index_text(title, 1, "S")
             indexer.index_text(title)
             indexer.increase_termpos()
             indexer.index_text(plot)
+            indexer.increase_termpos()
             indexer.index_text(actors)
+            indexer.increase_termpos()
+            indexer.index_text(directors)
+            x_doc.add_term("XYEAR{}".format(year))
+            #if value is uppercase, then add : between prefix and value
+            x_doc.add_term("XRATED:{}".format(_format_rated(rated)))
 
             # save
-            x_id = _index_id(imdb_id)
-            x_db.replace_document(x_id, x_doc)
-            print x_id
+            x_db.replace_document(rank, x_doc)
+
 
 if __name__ == '__main__':
     sys.exit(main())
